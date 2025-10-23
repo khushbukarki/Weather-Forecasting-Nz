f7dde2d1c6da9a3f96e4c7d38a5dfe3f935bd780
coderkk <64301266+khushbukarki@users.noreply.github.com>
2025-10-23 14:48:23 +1300

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -17,45 +17,45 @@ export const CITIES = [
     lat: -41.2866,
     lon: 174.7756,
     photo:
-      "https://upload.wikimedia.org/wikipedia/commons/8/80/Wellington_skyline_seen_from_Kelburn.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/7/76/Cityscape_of_Wellington.jpg",
     thumb:
-      "https://upload.wikimedia.org/wikipedia/commons/8/80/Wellington_skyline_seen_from_Kelburn.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/7/76/Cityscape_of_Wellington.jpg",
   },
   {
     name: "Christchurch",
     lat: -43.532,
     lon: 172.6306,
     photo:
-      "https://upload.wikimedia.org/wikipedia/commons/4/4e/Christchurch_city_skyline.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/7/7e/Christchurch_City.jpg",
     thumb:
-      "https://upload.wikimedia.org/wikipedia/commons/4/4e/Christchurch_city_skyline.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/7/7e/Christchurch_City.jpg",
   },
   {
     name: "Dunedin",
     lat: -45.8788,
     lon: 170.5028,
     photo:
-      "https://upload.wikimedia.org/wikipedia/commons/b/bb/Dunedin_city_skyline.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/3/3e/Dunedin_cbd_skyline.jpg",
     thumb:
-      "https://upload.wikimedia.org/wikipedia/commons/b/bb/Dunedin_city_skyline.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/3/3e/Dunedin_cbd_skyline.jpg",
   },
   {
     name: "Hamilton",
     lat: -37.787,
     lon: 175.2793,
     photo:
-      "https://upload.wikimedia.org/wikipedia/commons/7/76/Hamilton_New_Zealand_Skyline.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/f/f4/Hamilton_Gardens_-_Hamilton%2C_NZ_-_DSC06658.jpg",
     thumb:
-      "https://upload.wikimedia.org/wikipedia/commons/7/76/Hamilton_New_Zealand_Skyline.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/f/f4/Hamilton_Gardens_-_Hamilton%2C_NZ_-_DSC06658.jpg",
   },
   {
     name: "Tauranga",
     lat: -37.686,
     lon: 176.167,
     photo:
-      "https://upload.wikimedia.org/wikipedia/commons/9/9c/Mount_Maunganui_beach_Tauranga.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/5/54/New_Zealand%2C_Bay_of_Plenty%2C_Tauranga.jpg",
     thumb:
-      "https://upload.wikimedia.org/wikipedia/commons/9/9c/Mount_Maunganui_beach_Tauranga.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/5/54/New_Zealand%2C_Bay_of_Plenty%2C_Tauranga.jpg",
   },
 ];
 
