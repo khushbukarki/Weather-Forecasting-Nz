932dd3db59d8c84d4e829c7fc755079a64f3336c
coderkk <64301266+khushbukarki@users.noreply.github.com>
2025-10-23 13:09:53 +1300

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -5,54 +5,54 @@ export const CITIES = [
     lat: -36.8485,
     lon: 174.7633,
     photo:
-      "auckland.jpgq=80&w=1600&auto=format&fit=crop",
+      "auckland.jpg?q=80&w=1600&auto=format&fit=crop",
     thumb:
-      "auckland.jpgq=40&w=80&auto=format&fit=crop",
+      "auckland.jpg?q=40&w=80&auto=format&fit=crop",
   },
   {
     name: "Wellington",
     lat: -41.2866,
     lon: 174.7756,
     photo:
-      "wellington.jpgq=80&w=1600&auto=format&fit=crop",
+      "wellington.jpg?q=80&w=1600&auto=format&fit=crop",
     thumb:
-      "wellington.jpgq=40&w=80&auto=format&fit=crop",
+      "wellington.jpg?q=40&w=80&auto=format&fit=crop",
   },
   {
     name: "Christchurch",
     lat: -43.532,
     lon: 172.6306,
     photo:
-      "christchurch.jpgq=80&w=1600&auto=format&fit=crop",
+      "christchurch.jpg?q=80&w=1600&auto=format&fit=crop",
     thumb:
-      "christchurch.jpgq=40&w=80&auto=format&fit=crop",
+      "christchurch.jpg?q=40&w=80&auto=format&fit=crop",
   },
   {
     name: "Dunedin",
     lat: -45.8788,
     lon: 170.5028,
     photo:
-      "dunedin.jpgq=80&w=1600&auto=format&fit=crop",
+      "dunedin.jpg?q=80&w=1600&auto=format&fit=crop",
     thumb:
-      "dunedin.jpgq=40&w=80&auto=format&fit=crop",
+      "dunedin.jpg?q=40&w=80&auto=format&fit=crop",
   },
   {
     name: "Hamilton",
     lat: -37.787,
     lon: 175.2793,
     photo:
-      "hamilton.jpgq=80&w=1600&auto=format&fit=crop",
+      "hamilton.jpg?q=80&w=1600&auto=format&fit=crop",
     thumb:
-      "hamiltonq=40&w=80&auto=format&fit=crop",
+      "hamilton.jpg?q=40&w=80&auto=format&fit=crop",
   },
   {
     name: "Tauranga",
     lat: -37.6878,
     lon: 176.1651,
     photo:
-      "taurnaga.jpgq=80&w=1600&auto=format&fit=crop",
+      "taurnaga.jpg?q=80&w=1600&auto=format&fit=crop",
     thumb:
-      "taurnaga.jpgq=40&w=80&auto=format&fit=crop",
+      "taurnaga.jpg?q=40&w=80&auto=format&fit=crop",
   },
 ];
 
